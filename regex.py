b828d2a9d32e4504f075ca21b9107d7c305f8ec1
Orsan Awawdi <orsan.awawdi@gmail.com>
2023-12-07 10:02:33 +0200

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,21 +1,13 @@
 import re
 import time
 
-
 def timer_decorator(func):
-
     def wrapper_func(*args, **kwargs):
-
         start_time = time.time()
-
         result = func(*args, **kwargs)
-
         end_time = time.time()
-
         print(f"The time taken is: {end_time - start_time} seconds")
-
         return result
-
     return wrapper_func
 
 @timer_decorator
@@ -33,7 +25,6 @@ def squeeze_string(input_string: str) -> str:
 
 @timer_decorator
 def squeeze_using_re(input_string: str) -> str:
-    x = 10
     compressed = re.sub(r'(\w)\1*', lambda m : m[0][0] + str(len(m[0])), input_string)
     return compressed if len(compressed) < len(input_string) else input_string
 
